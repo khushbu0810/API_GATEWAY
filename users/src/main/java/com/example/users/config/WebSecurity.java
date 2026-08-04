c40e5685f5ecb50bbd31f40e145567f0114f2b21
Khushbu <Kahlwt08@gmail.com>
2026-08-04 09:46:10 +0530

    allowing request from api gateway only

--- users/src/main/java/com/example/users/config/WebSecurity.java
+++ users/src/main/java/com/example/users/config/WebSecurity.java
@@ -1,16 +1,25 @@
 package com.example.users.config;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.access.expression.WebExpressionAuthorizationManager;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class WebSecurity {
+
+    @Autowired
+    private Environment environment;
 
     @Bean
     protected SecurityFilterChain configure(HttpSecurity http) throws Exception {
@@ -19,15 +28,16 @@ public class WebSecurity {
                 .sessionManagement(session ->
                         session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/users").permitAll()
+                        .requestMatchers("/users")
+                        .access(new WebExpressionAuthorizationManager(
+                                "hasIpAddress('" + environment.getProperty("gateway.ip") + "')"))
                         .requestMatchers("/h2-console/**").permitAll()
                         .anyRequest().authenticated()
                 );
         http
                 .headers((headers) ->
                         headers
-                                .frameOptions((frameOptions) ->
-                                        frameOptions.sameOrigin()));
+                                .frameOptions(HeadersConfigurer.FrameOptionsConfig::sameOrigin));
         return http.build();
     }
 }
