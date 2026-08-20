01c2d2a9328f0d43dd6f3fce1a9b14460434a354
Khushbu <Kahlwt08@gmail.com>
2026-08-20 09:30:07 +0530

    Implemented PreFilter structure

---
+++ apiGateway/src/main/java/com/example/apiGateway/filter/MyPreFilter.java
@@ -0,0 +1,18 @@
+package com.example.apiGateway.filter;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+@Component
+@Slf4j
+public class MyPreFilter implements GlobalFilter {
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        log.info("My first PreFilter is executed.....");
+        return chain.filter(exchange);
+    }
+}
