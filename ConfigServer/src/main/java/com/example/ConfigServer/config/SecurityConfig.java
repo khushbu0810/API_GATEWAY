1622ee5f761a4cb6a358d460391d9f5307e6ee0c
Khushbu <Kahlwt08@gmail.com>
2026-08-22 12:07:45 +0530

    created user

--- ConfigServer/src/main/java/com/example/ConfigServer/config/SecurityConfig.java
+++ ConfigServer/src/main/java/com/example/ConfigServer/config/SecurityConfig.java
@@ -2,13 +2,28 @@ package com.example.ConfigServer.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import java.util.Objects;
 
 @Configuration
 public class SecurityConfig {
+
+    private final Environment environment;
+
+    public SecurityConfig(Environment environment) {
+        this.environment = environment;
+    }
+
     @Bean
     SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         //authentication for each user endpoints - Basic authentication
@@ -19,5 +34,22 @@ public class SecurityConfig {
                 .csrf(csrf -> csrf.ignoringRequestMatchers("/actuator/busrefresh"))
                 .httpBasic(Customizer.withDefaults());
         return http.build();
+    }
+
+    //creating users
+    @Bean
+    InMemoryUserDetailsManager userDetailsService(PasswordEncoder passwordEncoder) {
+        UserDetails admin = User
+                .withUsername(Objects.requireNonNull(environment.getProperty("spring.security.user.name")))
+                .password(passwordEncoder.encode(environment.getProperty("spring.security.user.password")))
+                .roles(Objects.requireNonNull(environment.getProperty("spring.security.user.roles")))
+                .build();
+
+        return new InMemoryUserDetailsManager(admin);
+    }
+
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
